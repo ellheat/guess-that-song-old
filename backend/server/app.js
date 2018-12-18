e647453c12058294962b59a77aee24d14a8fc3a7
Patryk Poleszczuk <patryk.poleszczuk@gmail.com>
2018-12-18 17:10:26 +0100

    Change config server

--- backend/server/app.js
+++ backend/server/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const port = require('../config/ports');
 const app = express();
+const router = express.Router(); // eslint-disable-line
 
 const { getCharacters } = require('../config/characters');
 const { getSpotifyToken, fetchPlaylist } = require('../utils/spotify');
@@ -9,6 +10,7 @@ const { getSpotifyToken, fetchPlaylist } = require('../utils/spotify');
 
 const configureServer = () => new Promise((resolve) => {
   app.use(express.static(path.join(__dirname, '../../build')));
+  app.use('/.netlify/functions/server', router);
   app.listen(port.api, () => {
     console.log(`Backend listening on port ${port.api}!`.success); // eslint-disable-line
     getCharacters().then(() => console.log('Characters created'.success)); // eslint-disable-line
